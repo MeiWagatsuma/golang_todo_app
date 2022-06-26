301dbfb84630a4bf6e2adc3b3c49a73ffb005703
MeiWagatsuma <wagamei256@gmail.com>
2022-06-26 17:43:41 +0900

    [add] Updateメソッドの作成

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	// defer Db.Close()
 
 	/*
+	// CREATE
 	u := &models.User{}
 	u.Name = "test"
 	u.Email = "test@example.com"
@@ -20,7 +21,18 @@ func main() {
 	u.CreateUser()
 	*/
 
+	/*
+	// READ
 	u, _ := models.GetUser(1)
+	fmt.Println(u)
+	*/
 
+	// UPDATE
+	u, _ := models.GetUser(1)
+	u.Name = "Test2"
+	u.Email = "test2@example.com"
+	u.UpdateUser()
+	// ↓確認用の出力
+	u, _ = models.GetUser(1)
 	fmt.Println(u)
 }
