c633e13d203e519dd119c0fca21fe78ad830113f
MeiWagatsuma <wagamei256@gmail.com>
2022-07-31 15:48:57 +0900

    [add] HTMLを返すように

--- main.go
+++ main.go
@@ -3,90 +3,13 @@ package main
 import (
 	"fmt"
 
+	"./app/controllers"
 	"./app/models"
 )
 
 func main() {
 	fmt.Println(models.Db)
-	// defer Db.Close()
 
-	////////////////////
-	// users
-	////////////////////
+	controllers.StartMainServer()
 
-	/*
-	// CREATE
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.Password = "password"
-	fmt.Println(u)
-
-	u.CreateUser()
-	*/
-
-	/*
-	// READ
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
-	*/
-
-	/*
-	// UPDATE
-	u, _ := models.GetUser(1)
-	u.Name = "Test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	// ↓確認用の出力
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
-	*/
-
-	/*
-	// DELETE
-	u, _ := models.GetUser(1)
-	u.DeleteUser()
-	fmt.Println(u)
-	*/
-
-	////////////////////
-	// todos
-	////////////////////
-	/*
-	// CREATE
-	user, _ := models.GetUser(2)
-	user.CreateTodo("FIrst Todo")
-	*/
-
-	// READ
-	// t, _ := models.GetTodo(1)
-	// fmt.Println(t)
-
-	/*
-	// Multiple READ 
-	todos, _ := models.GetTodos()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
-	*/
-
-	/*
-	// Multiple READ by User
-	user, _ := models.GetUser(3)
-	todo, _ := user.GetTodosByUser()
-	for _, v := range todo {
-		fmt.Println(v)
-	}
-	*/
-
-	/*
-	// UPDATE
-	t, _ := models.GetTodo(1)
-	t.Content = "Update Todo"
-	t.UpdateTodo()
-	*/
-
-	// DELTE
-	t, _ := models.GetTodo(3)
-	t.DeleteTodo()
 }
