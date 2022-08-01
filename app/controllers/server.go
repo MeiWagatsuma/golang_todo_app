7b61ef77440771ea649f96e8edca971dd2eff4c9
MeiWagatsuma <wagamei256@gmail.com>
2022-08-01 21:02:33 +0900

    [add] サインイン時とそうでないときとでページを変えるように

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"text/template"
 
+	"../../app/models"
 	"../../config"
 )
 
@@ -21,6 +22,17 @@ func generateHTML(w http.ResponseWriter, data interface{}, filename ...string) {
 	}
 }
 
+func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err error) {
+	cookie, err := r.Cookie("_cookie")
+	if err == nil {
+		sess = models.Session{UUID: cookie.Value}
+		if ok, _ := sess.CheckSession(); !ok {
+			err = fmt.Errorf("Invalid session")
+		}
+	}
+	return sess, err
+}
+
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static", files))
@@ -29,6 +41,7 @@ func StartMainServer() error {
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/authenticate", authenticate)
+	http.HandleFunc("/todos", index)
 
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
