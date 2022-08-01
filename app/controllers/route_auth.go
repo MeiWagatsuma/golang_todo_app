3902f5ffae8c1de284ebffa4991de220f2fd8ce7
MeiWagatsuma <wagamei256@gmail.com>
2022-08-01 19:01:43 +0900

    [add] sessionを使ったlogin機能の実装

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -7,9 +7,9 @@ import (
 	"../../app/models"
 )
 
-func signup(w http.ResponseWriter, r *http.Request){
+func signup(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		generateHTML(w, nil, "layout","public_navbar", "signup")
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
 	} else if r.Method == "POST" {
 		err := r.ParseForm()
 		if err != nil {
@@ -17,8 +17,8 @@ func signup(w http.ResponseWriter, r *http.Request){
 
 		}
 		user := models.User{
-			Name: r.PostFormValue("name"),
-			Email: r.PostFormValue("email"),
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
 			Password: r.PostFormValue("password"),
 		}
 		if err := user.CreateUser(); err != nil {
@@ -30,4 +30,30 @@ func signup(w http.ResponseWriter, r *http.Request){
 
 func login(w http.ResponseWriter, r *http.Request) {
 	generateHTML(w, nil, "layout", "public_navbar", "login")
+}
+
+func authenticate(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	user, err := models.GetUserByEmail(r.PostFormValue("email"))
+	if err != nil {
+		log.Println(err)
+		http.Redirect(w, r, "/login", 302)
+	}
+	if user.Password == models.Encrypt(r.PostFormValue("password")) {
+		session, err := user.CreateSession()
+		if err != nil {
+			log.Println(err)
+		}
+
+		cookie := http.Cookie{
+			Name:     "_cookie",
+			Value:    session.UUID,
+			HttpOnly: true,
+		}
+		http.SetCookie(w, &cookie)
+
+		http.Redirect(w, r, "/", 302)
+	} else {
+		http.Redirect(w, r, "/login", 302)
+	}
 }
